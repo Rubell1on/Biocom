264887c67ddf0e5e2feb7816f035a18c70531e4d
Khokhlove <52717700+Khokhlove@users.noreply.github.com>
2021-08-05 14:32:50 +0700

    Added 3DPanel

--- Assets/CameraRotateAround.cs
+++ Assets/CameraRotateAround.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CameraRotateAround : MonoBehaviour {
+public class CameraRotateAround : MonoBehaviour 
+{
 
+	public Transform cameraTransform;
 	public Transform target;
 	public Vector3 offset;
 	public float sensitivity = 3; // чувствительность мышки
@@ -13,36 +15,35 @@ public class CameraRotateAround : MonoBehaviour {
 	public float zoomMax = 10; // макс. увеличение
 	public float zoomMin = 3; // мин. увеличение
 	private float X, Y;
-	public RectTransform rect;
+
+	private BoxCollider2D coliderPanel;
 
 
 	void Start()
 	{
-		rect = GetComponentInParent<RectTransform>();
-
+		coliderPanel = GetComponent<BoxCollider2D>();
+		coliderPanel.size = GetComponent<RectTransform>().rect.size;
+		
 		limit = Mathf.Abs(limit);
+
 		if (limit > 90) limit = 90;
 		offset = new Vector3(offset.x, offset.y, -Mathf.Abs(zoomMax) / 2);
-		transform.position = target.position + offset;
+		cameraTransform.position = target.position + offset;
 	}
 
-	void Update()
-	{
+    private void OnMouseOver()
+    {
 		if (Input.GetMouseButton(0))
-		{ 
-			Rect targetrect = new Rect(rect.anchoredPosition.x, rect.anchoredPosition.y, rect.sizeDelta.x, rect.sizeDelta.y);
-			if (targetrect.Contains(Input.mousePosition))
-			{
-				if (Input.GetAxis("Mouse ScrollWheel") > 0) offset.z += zoom;
-				else if (Input.GetAxis("Mouse ScrollWheel") < 0) offset.z -= zoom;
-				offset.z = Mathf.Clamp(offset.z, -Mathf.Abs(zoomMax), -Mathf.Abs(zoomMin));
+		{
+			if (Input.GetAxis("Mouse ScrollWheel") > 0) offset.z += zoom;
+			else if (Input.GetAxis("Mouse ScrollWheel") < 0) offset.z -= zoom;
+			offset.z = Mathf.Clamp(offset.z, -Mathf.Abs(zoomMax), -Mathf.Abs(zoomMin));
 
-				X = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
-				Y += Input.GetAxis("Mouse Y") * sensitivity;
-				Y = Mathf.Clamp(Y, -limit, limit);
-				transform.localEulerAngles = new Vector3(-Y, X, 0);
-				transform.position = transform.localRotation * offset + target.position;
-			}
+			X = cameraTransform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+			Y += Input.GetAxis("Mouse Y") * sensitivity;
+			Y = Mathf.Clamp(Y, -limit, limit);
+			cameraTransform.localEulerAngles = new Vector3(-Y, X, 0);
+			cameraTransform.position = cameraTransform.localRotation * offset + target.position;
 		}
 	}
 
