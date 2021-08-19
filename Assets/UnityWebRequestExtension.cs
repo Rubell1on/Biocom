7b4e618c9e543e6cf8bf343c28ba86b93d8d27c2
rubell1on <dablaev@yandex.ru>
2021-08-19 16:55:09 +0700

    Рефакторинг

---
+++ Assets/UnityWebRequestExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public static class UnityWebRequestExtension
+{
+    public static TaskAwaiter GetAwaiter(this AsyncOperation asyncOp)
+    {
+        var tcs = new TaskCompletionSource<object>();
+        asyncOp.completed += obj => { tcs.SetResult(null); };
+        return ((Task)tcs.Task).GetAwaiter();
+    }
+}
